4936335fdbe71ae9de0d9aca318bb70ed90dbda1
杜雪迎 <702699388@qq.com>
2020-03-02 13:43:43 +0800

    栈的压入弹出序列

---
+++ StackPush.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Stack;
+public class StackPush {
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        if(pushA.length==0||popA.length==0){
+            return false;
+        }
+        Stack<Integer> stack = new Stack<Integer>();
+        int j=0;
+        for(int i=0;i<popA.length;i++){
+            stack.push(pushA[i]);
+            while(j<popA.length&&stack.peek()==popA[j]){
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.empty()?true:false;
+    }
+}
+
